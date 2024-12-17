d12c2ecb0b57d5e6fdb0c4a0f5ec6200b46e8150
Eduardo <ealvarado261500@gmail.com>
2024-12-17 12:54:29 -0600

    sm8450-common: Move few kernel scripts to device tree

--- common.mk
+++ common.mk
@@ -261,6 +261,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.default \
     init.class_main.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-taro.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -269,6 +271,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc

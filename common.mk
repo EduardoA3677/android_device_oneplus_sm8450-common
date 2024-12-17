511b12f37ee46de2b8a6b10da90b32d247dcb4e4
Eduardo <ealvarado261500@gmail.com>
2024-12-17 12:29:57 -0600

    sm8450-common: Inherit vabc_features.mk and update compression to lz4

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@
 TARGET_DISABLE_EPPE := true
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -29,6 +29,8 @@ PRODUCT_PACKAGES += \
     libcodec2_shim \
     libgui_shim \
     libhidlbase_shim
+
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
 # ANT+
 PRODUCT_PACKAGES += \

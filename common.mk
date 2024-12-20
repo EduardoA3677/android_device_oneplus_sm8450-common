4df3dc388f8aacf0b392c50e1a2baee497510a6e
Eduardo <ealvarado261500@gmail.com>
2024-12-20 00:06:54 -0600

    Revert "sm8450-common: Update audio configuration and replace libhfp_pal with libfmpal"
    This reverts commit c77f1b86e64746fae9f19676f013f2d252d5ce22.

--- common.mk
+++ common.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
     libbatterylistener \
-    libfmpal \
+    libhfp_pal \
     libpalclient \
     libqcomvisualizer \
     libqcomvoiceprocessing \

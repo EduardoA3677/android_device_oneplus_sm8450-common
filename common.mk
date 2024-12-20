1b4844cd60a9e8876482d3886036a611afc8cc6e
Eduardo <ealvarado261500@gmail.com>
2024-12-19 21:38:30 -0600

    sm8450-common: Don't explicitly include updatable_apex.mk

--- common.mk
+++ common.mk
@@ -37,9 +37,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
